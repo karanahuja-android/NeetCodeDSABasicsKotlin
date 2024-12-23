6e93edc558de4c86610577f6597d0027a4ca6105
karanahuja-android <karan26.ahuja@gmail.com>
2024-12-23 23:59:25 +0530

    add

--- NeetcodeProDSABasicsKotlin/src/Sorting/BucketSort.kt
+++ NeetcodeProDSABasicsKotlin/src/Sorting/BucketSort.kt
@@ -2,6 +2,20 @@ package Sorting
 
 class BucketSort {
     fun sort(input:IntArray){
+        val counts = intArrayOf(0,0,0)
+        input.forEach{
+            when{
+                it == 0 ->counts[0] +=1
+                it  == 1 -> counts[1] += 1
+                it == 2 -> counts[2] += 1
+            }
+            var k = 0
+            for(i in 0..2)
+                for(j in 0 until counts[i]) {
+                    input[k] = i
+                    k++
+                }
 
+        }
     }
 }
